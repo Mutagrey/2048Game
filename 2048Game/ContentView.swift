3e5e4324a3144c4832c8197039f7b27e68f33aeb
Sergey Petrov <sergeypetrovmtgrey@gmail.com>
2022-09-10 17:51:22 +0300

    Initial Commit

---
+++ 2048Game/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  2048Game
+//
+//  Created by Sergey Petrov on 10.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2048Game/_048GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  _048GameApp.swift
+//  2048Game
+//
+//  Created by Sergey Petrov on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _048GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
